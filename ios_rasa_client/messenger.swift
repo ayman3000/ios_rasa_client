2e5a5ba32cfae1242b3ee9064072cad18fe724c4
ayman3000 <ayman3000@gmail.com>
2023-04-02 11:47:57 +0200

    init commit

---
+++ ios_rasa_client/messenger.swift
@@ -0,0 +1,20 @@
+//
+//  messenger.swift
+//  ios_rasa_client
+//
+//  Created by ayman moustafa on 02/04/2023.
+//
+
+import SwiftUI
+
+struct messenger: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct messenger_Previews: PreviewProvider {
+    static var previews: some View {
+        messenger()
+    }
+}
