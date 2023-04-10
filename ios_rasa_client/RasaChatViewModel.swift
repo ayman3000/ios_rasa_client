d33691f5a3eb77d69b3b80a6bb49e3e41c51dc9c
ayman3000 <ayman3000@gmail.com>
2023-04-10 15:07:36 +0200

    fix speaking

--- ios_rasa_client/RasaChatViewModel.swift
+++ ios_rasa_client/RasaChatViewModel.swift
@@ -10,31 +10,36 @@ import SocketIO
 import SwiftUI
 import AVFoundation
 
-class RasaChatViewModel: ObservableObject {
+class RasaChatViewModel:  ObservableObject {
     @Published var messages: [ChatMessage] = []
     @Published var isConnected = false
 
     private var manager: SocketManager!
     private var socket: SocketIOClient!
-    private let speechSynthesizer = AVSpeechSynthesizer()
+//    private let speechSynthesizer = AVSpeechSynthesizer()
+    let speaker = Speaker()
 
     init() {
         
-        let audioSession = AVAudioSession.sharedInstance()
-        do {
-//            try audioSession.setCategory(.playback, mode: .default)
-//            try audioSession.setCategory(.playAndRecord, mode: .spokenAudio, options: [.defaultToSpeaker, .allowBluetooth, .allowAirPlay])
-            try audioSession.setActive(true)
-        } catch {
-            print(" Failed to set up audio session: \(error)")
-        }
+//        let audioSession = AVAudioSession.sharedInstance()
+//        do {
+////            try audioSession.setCategory(.playback, mode: .default)
+////            try audioSession.setCategory(.playAndRecord, mode: .spokenAudio, options: [.defaultToSpeaker, .allowBluetooth, .allowAirPlay])
+//            try audioSession.setActive(true)
+//        } catch {
+//            print(" Failed to set up audio session: \(error)")
+//        }
+        
+        self.speaker.test(msg: "test")
+    
+          
         setupSocket()
         connect()
     }
 
     func setupSocket() {
         // Replace `http://localhost:5005` with your Rasa server URL
-        manager = SocketManager(socketURL: URL(string: "http://192.168.1.5:5005")!, config: [.log(false), .compress])
+        manager = SocketManager(socketURL: URL(string: "http://172.19.178.48:5005")!, config: [.log(false), .compress])
         socket = manager.defaultSocket
     }
 
@@ -52,8 +57,11 @@ class RasaChatViewModel: ObservableObject {
             if let response = data.first as? [String: Any], let message = response["text"] as? String {
                 DispatchQueue.main.async {
                     self.messages.append(ChatMessage(sender: .bot, text: message))
-                    self.speak(text: message)
+//                    self.speak(text: message)
+                    
+                    
                 }
+                self.speaker.speak(message, language: "en-US")
             }
         }
 
@@ -70,20 +78,72 @@ class RasaChatViewModel: ObservableObject {
 
         socket.emit("user_uttered", ["message": text])
     }
-    
-    private func speak(text: String) {
-        let speechUtterance = AVSpeechUtterance(string: text)
-        if let voice = AVSpeechSynthesisVoice(identifier: "com.apple.ttsbundle.Samantha-compact") {
-            speechUtterance.voice = voice
-        }
-        else {
-            speechUtterance.voice = AVSpeechSynthesisVoice(language: "en-US")
-            
-        }
-        speechSynthesizer.speak(speechUtterance)
-    }
+//    private func speak(text: String) {
+//        let speechUtterance = AVSpeechUtterance(string: text)
+//        speechUtterance.volume = 2
+////        if let voice = AVSpeechSynthesisVoice(identifier: "com.apple.ttsbundle.Samantha-compact") {
+////            speechUtterance.voice = voice
+////        }
+////        else {
+////            speechUtterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+////
+////        }
+//        speechUtterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+//
+//        speaker.speak(msg: speechUtterance)
+////        DispatchQueue.main.async {
+////            self.speechSynthesizer.speak(speechUtterance)
+////               }
+//
+//    }
 
 }
+
+
+class Speaker: NSObject {
+    let synthesizer = AVSpeechSynthesizer()
+    override init() {
+        super.init()
+       synthesizer.delegate = self
+    }
+
+    func speak(_ text: String, language: String) {
+            do {
+                let utterance = AVSpeechUtterance(string: text)
+                
+                if let voice = AVSpeechSynthesisVoice(identifier: "com.apple.ttsbundle.siri_male_en-US_compact") {
+                    utterance.voice = voice
+                     }
+                     else {
+                         utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+             
+                     }
+                   
+//                utterance.voice = voice
+                
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                try AVAudioSession.sharedInstance().setActive(true)
+                self.synthesizer.speak(utterance)
+            } catch let error {
+//                self.errorDescription = error.localizedDescription
+//                isShowingSpeakingErrorAlert.toggle()
+                
+                print( error.localizedDescription)
+            }
+        }
+    
+    func test(msg: String){
+        self.speak("test", language: "en-US")
+    }
+}
+extension Speaker: AVSpeechSynthesizerDelegate {
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        try? AVAudioSession.sharedInstance().setActive(false, options: .notifyOthersOnDeactivation)
+
+        print("all done")
+    }
+}
+
 
 
 
